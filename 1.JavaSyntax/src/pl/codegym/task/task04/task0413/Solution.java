e7ab1be78a64f195865e560954fa46929bdb1d2d
MichalRumiancew <michalr11@gmail.com>
2022-09-27 21:54:46 +0200

    tasks0412 two different solutions.

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0413/Solution.java
@@ -0,0 +1,39 @@
+package pl.codegym.task.task04.task0413;
+
+/* 
+Dzień tygodnia
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int day = scanner.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Poniedziałek");
+                break;
+            case 2:
+                System.out.println("Wtorek");
+                break;
+            case 3:
+                System.out.println("Środa");
+                break;
+            case 4:
+                System.out.println("Czwartek");
+                break;
+            case 5:
+                System.out.println("Piątek");
+                break;
+            case 6:
+                System.out.println("Sobota");
+                break;
+            case 7:
+                System.out.println("Niedziela");
+                break;
+            default:
+                System.out.println("Nie ma takiego dnia tygodnia");
+        }
+    }
+}
