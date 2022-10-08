e473586688e0d0a04d1e62e66098906be2b0906f
MichalRumiancew <michalr11@gmail.com>
2022-10-08 18:27:52 +0200

    10x10

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0433/Solution.java
@@ -0,0 +1,24 @@
+package pl.codegym.task.task04.task0433;
+
+
+/* 
+Zobaczyć dolary w przyszłości
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        int i = 0;
+
+        while (i < 10) {
+            int j = 0;
+            while (j < 10) {
+                System.out.print("$");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+
+    }
+}
