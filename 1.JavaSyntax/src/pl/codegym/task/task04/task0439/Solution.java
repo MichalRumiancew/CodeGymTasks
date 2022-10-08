1f44e2fc663f38c8ee052428ad16a5e6a94bdd9f
MichalRumiancew <michalr11@gmail.com>
2022-10-08 19:00:56 +0200

    439

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0439/Solution.java
@@ -0,0 +1,20 @@
+package pl.codegym.task.task04.task0439;
+
+/* 
+Łańcuszek
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name + " mnie kocha.");
+
+        }
+
+    }
+}
