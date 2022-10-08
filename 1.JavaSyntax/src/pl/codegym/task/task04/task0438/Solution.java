46713e52b324be6ae6847a05e39ed3ee86838090
MichalRumiancew <michalr11@gmail.com>
2022-10-08 18:57:20 +0200

    437
    8
    88
    888
    8888
    88888
    888888
    8888888
    88888888
    888888888
    8888888888

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0438/Solution.java
@@ -0,0 +1,22 @@
+package pl.codegym.task.task04.task0438;
+
+/* 
+Rysowanie linii
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+
+        //horizontal line
+        for (int j = 0; j < 10; j++) {
+            System.out.print('8');
+        }
+        System.out.println();
+
+        //vertical line
+        for (int i = 0; i < 10; i++) {
+            System.out.println('8');
+        }
+    }
+}
