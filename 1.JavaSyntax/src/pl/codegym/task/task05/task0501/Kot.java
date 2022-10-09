022bd20a642d286fa309ea0c2026d0d069cee3f6
MichalRumiancew <michalr11@gmail.com>
2022-10-09 12:44:37 +0200

    classes

---
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0501/Kot.java
@@ -0,0 +1,16 @@
+package pl.codegym.task.task05.task0501;
+
+/* 
+Tworzenie kota
+*/
+
+public class Kot {
+    public String imie;
+    protected int wiek;
+    int waga;
+    private int sila;
+
+    public static void main(String[] args) {
+
+    }
+}
