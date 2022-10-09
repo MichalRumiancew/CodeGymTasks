4b411c0e55af169bba652e0922a0f0f8ce523274
MichalRumiancew <michalr11@gmail.com>
2022-10-09 18:14:49 +0200

    505 cats fight

---
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0505/Solution.java
@@ -0,0 +1,41 @@
+package pl.codegym.task.task05.task0505;
+
+/* 
+Kocia jatka
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Kot kot1 = new Kot("Alan",1,4,5);
+        Kot kot2 = new Kot("BBB",3,4,3);
+        Kot kot3 = new Kot("CCC",6,3,5);
+
+        System.out.println(kot1.walka(kot2));
+        System.out.println(kot1.walka(kot3));
+        System.out.println(kot2.walka(kot3));
+
+    }
+
+    public static class Kot {
+        protected String imie;
+        protected int wiek;
+        protected int waga;
+        protected int sila;
+
+        public Kot(String imie, int wiek, int waga, int sila) {
+            this.imie = imie;
+            this.wiek = wiek;
+            this.waga = waga;
+            this.sila = sila;
+        }
+
+        public boolean walka(Kot innyKot) {
+            int wiekPrzewaga = this.wiek > innyKot.wiek ? 1 : 0;
+            int wagaPrzewaga = this.waga > innyKot.waga ? 1 : 0;
+            int silaPrzewaga = this.sila > innyKot.sila ? 1 : 0;
+
+            int wynik = wiekPrzewaga + wagaPrzewaga + silaPrzewaga;
+            return wynik > 2; // return wynik > 2 ? true : false;
+        }
+    }
+}
