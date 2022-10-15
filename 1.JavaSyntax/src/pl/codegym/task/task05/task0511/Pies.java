c6abbe3ad31aa4eb4bcbdbc4eec4dec10ea243f9
MichalRumiancew <michalr11@gmail.com>
2022-10-15 13:19:09 +0200

    518 and 519

---
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0511/Pies.java
@@ -0,0 +1,32 @@
+package pl.codegym.task.task05.task0511;
+
+/* 
+Utwórz klasę Pies
+*/
+
+public class Pies {
+    public String imie;
+    public int wzrost;
+    public String kolor;
+
+    public void inicjalizuj(String imie) {
+        this.imie = imie;
+    }
+
+    public void inicjalizuj(String imie, int wzrost) {
+        this.imie = imie;
+        this.wzrost = wzrost;
+    }
+
+    public void inicjalizuj(String imie, int wzrost, String kolor) {
+        this.imie = imie;
+        this.wzrost = wzrost;
+        this.kolor = kolor;
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
