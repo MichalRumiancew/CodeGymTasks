a522965ec2d821c0e340fb751b5253f0b24b3168
MichalRumiancew <michalr11@gmail.com>
2022-10-16 17:01:20 +0200

    515

---
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0516/Przyjaciel.java
@@ -0,0 +1,31 @@
+package pl.codegym.task.task05.task0516;
+
+/* 
+Przyjaciół nie da się kupić
+*/
+
+public class Przyjaciel {
+    public String imie;
+    public int wiek;
+    public char plec;
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public Przyjaciel(String imie) {
+        this.imie = imie;
+    }
+
+    public Przyjaciel(String imie, int wiek) {
+        this.imie = imie;
+        this.wiek = wiek;
+    }
+
+    public Przyjaciel(String imie, int wiek, char plec) {
+        this.imie = imie;
+        this.wiek = wiek;
+        this.plec = plec;
+    }
+}
