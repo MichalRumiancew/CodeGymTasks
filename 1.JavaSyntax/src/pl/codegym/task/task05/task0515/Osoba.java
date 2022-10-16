f707ed13ff8dca9dbd666359309739ea68b3a2ec
MichalRumiancew <michalr11@gmail.com>
2022-10-16 12:49:04 +0200

    515

---
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0515/Osoba.java
@@ -0,0 +1,27 @@
+package pl.codegym.task.task05.task0515;
+
+/* 
+Inicjalizowanie obiektów
+*/
+
+public class Osoba {
+
+    String imie;
+    char plec;
+    int pieniadze;
+    int waga;
+    double rozmiar;
+
+
+    public void inicjalizuj(String imie, int pieniadze, char plec, int waga, double rozmiar) {
+        this.imie = imie;
+        this.pieniadze = pieniadze;
+        this.plec = plec;
+        this.imie = imie;
+        this.rozmiar = rozmiar;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
