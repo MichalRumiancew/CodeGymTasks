81697cadb008c5f23d64afe176010f88796d255f
MichalRumiancew <michalr11@gmail.com>
2022-10-18 19:54:55 +0200

    527

--- 1.JavaSyntax/src/pl/codegym/task/task05/task0528/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0528/Solution.java
@@ -1,11 +1,15 @@
 package pl.codegym.task.task05.task0528;
 
-/* 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*
 Wyświetl dzisiejszą datę
 */
 
 public class Solution {
     public static void main(String[] args) {
-        //tutaj wpisz swój kod
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM dd yyyy");
+        System.out.println(simpleDateFormat.format(new Date()));
     }
 }
