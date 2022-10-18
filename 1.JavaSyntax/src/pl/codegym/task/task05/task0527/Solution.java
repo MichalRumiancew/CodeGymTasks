5f986857abcd92cf1ecb8caa3e94f47500c97324
MichalRumiancew <michalr11@gmail.com>
2022-10-18 19:48:34 +0200

    527

--- 1.JavaSyntax/src/pl/codegym/task/task05/task0527/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0527/Solution.java
@@ -7,6 +7,9 @@ Tom i Jerry
 public class Solution {
     public static void main(String[] args) {
         Mysz jerryMysz = new Mysz("Jerry", 12, 5);
+        Kot tomKot = new Kot("Tom", 12, 5);
+        Pies azorPies = new Pies("Azor", 12, 5);
+
 
         //tutaj wpisz swój kod
     }
@@ -18,6 +21,32 @@ public class Solution {
         int ogon;
 
         public Mysz(String imie, int wzrost, int ogon) {
+            this.imie = imie;
+            this.wzrost = wzrost;
+            this.ogon = ogon;
+        }
+    }
+
+    public static class Kot {
+
+        String imie;
+        int wzrost;
+        int ogon;
+
+        public Kot(String imie, int wzrost, int ogon) {
+            this.imie = imie;
+            this.wzrost = wzrost;
+            this.ogon = ogon;
+        }
+    }
+
+    public static class Pies {
+
+        String imie;
+        int wzrost;
+        int ogon;
+
+        public Pies(String imie, int wzrost, int ogon) {
             this.imie = imie;
             this.wzrost = wzrost;
             this.ogon = ogon;
