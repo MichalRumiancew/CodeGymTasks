4e17fb776beb2967ddde154aa8462c813c5f9474
MichalRumiancew <michalr11@gmail.com>
2022-11-04 18:20:48 +0100

    607

---
+++ 1.JavaSyntax/src/pl/codegym/task/task06/task0607/Kot.java
@@ -0,0 +1,17 @@
+package pl.codegym.task.task06.task0607;
+
+/* 
+Licznik klasy
+*/
+
+public class Kot {
+    private static int licznikKotow = 0;
+
+    public Kot() {
+        licznikKotow++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
