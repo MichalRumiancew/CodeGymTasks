aa78106424e3d98f8f9be23bc03b52c31046a24b
MichalRumiancew <michalr11@gmail.com>
2022-11-06 11:19:12 +0100

    615

--- 1.JavaSyntax/src/pl/codegym/task/task06/task0615/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task06/task0615/Solution.java
@@ -6,9 +6,9 @@ Feng Shui i statyczne
 
 public class Solution {
 
-    public int A = 5;
+    public static int A = 5;
     public int B = 2;
-    public static int C = A * B;
+    public int C = A * B;
 
     public static void main(String[] args) {
         A = 15;
