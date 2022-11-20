4f9f41724103ce68d44a9d35cede34e58e184b08
MichalRumiancew <michalr11@gmail.com>
2022-11-20 12:30:48 +0100

    set

--- 1.JavaSyntax/src/pl/codegym/task/task08/task0811/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task08/task0811/Solution.java
@@ -10,22 +10,22 @@ Kwartet metod
 
 public class Solution {
     public static List getListaDlaGet() {
-        //tutaj wpisz swój kod
+        return new ArrayList();
 
     }
 
     public static List getListaDlaSet() {
-        //tutaj wpisz swój kod
+        return new ArrayList();
 
     }
 
     public static List getListaDlaDodajLubWstaw() {
-        //tutaj wpisz swój kod
+        return new LinkedList();
 
     }
 
     public static List getListaDlaUsun() {
-        //tutaj wpisz swój kod
+        return new LinkedList();
 
     }
 
--- 1.JavaSyntax/src/pl/codegym/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task08/task0812/Solution.java
@@ -1,15 +1,42 @@
 package pl.codegym.task.task08.task0812;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-/* 
+/*
 Najdłuższa sekwencja
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        //tutaj wpisz swój kod
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            String s = reader.readLine();
+            list.add(Integer.parseInt(s));
+        }
+
+        int count = 1;
+        int max = 1;
+        for (int i = 0; i < 9; i++) {
+            if (list.get(i).equals(list.get(i + 1))) {
+                max++;
+            } else if (max > count) {
+                count = max;
+                max = 1;
+            } else {
+                max = 1;
+            }
+        }
+
+        if (max > count) {
+            count = max;
+        }
+
+        System.out.println(count);
     }
 }
