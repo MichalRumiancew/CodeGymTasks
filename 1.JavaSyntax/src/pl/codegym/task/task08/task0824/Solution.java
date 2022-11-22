1607f5d4ae053d074b08cf44957e41fa51c36222
MichalRumiancew <michalr11@gmail.com>
2022-11-22 17:34:55 +0100

    tworzy ludzie i uzupełnia dane

--- 1.JavaSyntax/src/pl/codegym/task/task08/task0824/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task08/task0824/Solution.java
@@ -1,16 +1,59 @@
 package pl.codegym.task.task08.task0824;
-//
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 /* 
 Załóż rodzinę
 */
 
 public class Solution {
     public static void main(String[] args) {
-        //tutaj wpisz swój kod
+        Ludzie c1 = new Ludzie("C1", true, 15);
+        Ludzie c2 = new Ludzie("C2", false, 10);
+        Ludzie c3 = new Ludzie("C3", true, 5);
+
+        Ludzie f = new Ludzie("F", true, 33, c1, c2, c3);
+        Ludzie m = new Ludzie("M", false, 29, c1, c2, c3);
+
+        Ludzie grandF1 = new Ludzie("GF1", true, 69, f);
+        Ludzie grandM1 = new Ludzie("GM1", false, 65, f);
+
+        Ludzie grandF2 = new Ludzie("GF2", true, 63, m);
+        Ludzie grandM2 = new Ludzie("GM2", false, 62, m);
+
+
+        System.out.println(grandF1);
+        System.out.println(grandM1);
+        System.out.println(grandF2);
+        System.out.println(grandM2);
+        System.out.println(f);
+        System.out.println(m);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
     }
 
     public static class Ludzie {
-        //tutaj wpisz swój kod
+        String imie;
+        boolean plec;
+        int wiek;
+
+        ArrayList<Ludzie> dzieci = new ArrayList<>();
+
+        public Ludzie(String imie, boolean plec, int wiek) {
+            this.imie = imie;
+            this.plec = plec;
+            this.wiek = wiek;
+        }
+
+        public Ludzie(String imie, boolean plec, int wiek, Ludzie... dzieci) {
+            this.imie = imie;
+            this.plec = plec;
+            this.wiek = wiek;
+
+            Collections.addAll(this.dzieci, dzieci);
+        }
 
         public String toString() {
             String tekst = "";
